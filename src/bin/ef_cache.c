f710cb7277f21f729638ef020f4ae227a652f796
Jan Luebbe <jluebbe@debian.org>
2008-02-28 00:41:03 +0100

    Import upstream snapshot 0.0.3.006

---
+++ src/bin/ef_cache.c
@@ -0,0 +1,45 @@
+#include "Efreet.h"
+#include <stdio.h>
+
+static int
+timer(void *data)
+{
+    Ecore_List *list;
+    Efreet_Desktop *desktop;
+    double start;
+
+    start = ecore_time_get();
+    desktop = efreet_util_desktop_name_find("Mozilla Firefox");
+    printf("Mozilla Firefox: %p %.3f\n", desktop, (ecore_time_get() - start));
+    start = ecore_time_get();
+    desktop = efreet_util_desktop_generic_name_find("Mail/News");
+    printf("Mail/News: %p %.3f\n", desktop, (ecore_time_get() - start));
+    start = ecore_time_get();
+    desktop = efreet_util_desktop_wm_class_find("Mozilla-firefox-bin", NULL);
+    printf("Mozilla-firefox-bin: %p %.3f\n", desktop, (ecore_time_get() - start));
+
+    list = efreet_util_desktop_mime_list("application/ogg");
+    if (list)
+    {
+        ecore_list_first_goto(list);
+        while ((desktop = ecore_list_next(list)))
+        {
+            printf("application/ogg: %s\n", desktop->name);
+        }
+        ecore_list_destroy(list);
+    }
+
+    return 0;
+}
+
+int
+main(int argc, char **argv)
+{
+    if (!efreet_init()) return 1;
+    if (!efreet_util_init()) return 1;
+    ecore_timer_add(2.0, timer, NULL);
+    ecore_main_loop_begin();
+    efreet_util_shutdown();
+    efreet_shutdown();
+    return 0;
+}
---
+++ src/bin/ef_utils.c
@@ -0,0 +1,33 @@
+/* vim: set sw=4 ts=4 sts=4 et: */
+#include "Efreet.h"
+#include "efreet_private.h"
+
+int
+ef_cb_utils(void)
+{
+    Efreet_Desktop *desktop;
+    char *tmp1;
+    const char *tmp2;
+
+    if (!efreet_util_init()) return 0;
+    printf("\n");
+    tmp1 = efreet_util_path_in_default("applications",
+            "/usr/share/applications/test.desktop");
+    if (tmp1)
+    {
+        printf("%s\n", tmp1);
+        free(tmp1);
+    }
+
+    tmp2 = efreet_util_path_to_file_id("/usr/share/applications/this/tmp/test.desktop");
+    if (tmp2) printf("%s\n", tmp2);
+
+    desktop = efreet_util_desktop_file_id_find("kde-kresources.desktop");
+    printf("kde-kresources.desktop: %p\n", desktop);
+    desktop = efreet_util_desktop_file_id_find("mplayer.desktop");
+    printf("mplayer.desktop: %p\n", desktop);
+    desktop = efreet_util_desktop_file_id_find("nautilus-computer.desktop");
+    printf("nautilus-computer.desktop: %p\n", desktop);
+    efreet_util_shutdown();
+    return 1;
+}
